3014f9eda7a75e1608f5a2023af0e5c5a86001a2
xbnewbie <fariz0020@students.amikom.ac.id>
2017-10-02 11:16:57 +0700

    senin

--- test.py
+++ test.py
@@ -8,8 +8,15 @@ import difflib
 from docx import Document
 from docx.shared import Inches
 import  docx2txt
+import wx
+
 
 done = False
+ratio_config = 0.6
+
+debug_mode = False
+
+
 #here is the animation
 def animate():
     for c in itertools.cycle(['|', '/', '-', '\\']):
@@ -18,7 +25,8 @@ def animate():
         sys.stdout.write('\rMemproses file ' + c)
         sys.stdout.flush()
         time.sleep(0.1)
-    sys.stdout.write('\rDone!     ')
+    sys.stdout.write('\rDone!     \n')
+    exit = raw_input("press any key to exit")
 
 def create_schedule(list):
     """ Create a schedule for the teams in the list and return it"""
@@ -44,13 +52,16 @@ def create_schedule(list):
     return s
 
 
-
-if(len(sys.argv) ==1):
-    print "direktori tidak ada"
-    exit(1)
+app = wx.App()
+print "Pilih directori tugas "
+dialog = wx.DirDialog(None, "Pilih directori tugas:",style=wx.DD_DEFAULT_STYLE | wx.DD_NEW_DIR_BUTTON)
+if dialog.ShowModal() == wx.ID_OK:
+    direktori= dialog.GetPath()
 else:
-    direktori = sys.argv[1]
-    print direktori
+    print ""
+    exit()
+dialog.Destroy()
+
 ##listing file in direktori
 data_file ={}
 list_content={}
@@ -58,7 +69,7 @@ tugas =[]
 list_file = os.listdir(direktori);
 
 for file in list_file:
-    if(file.endswith(".docx")):
+    if(file.endswith(".docx") or file.endswith(".doc")):
         full_path =os.path.join(direktori,file)
         tugas.append(full_path)
 
@@ -66,35 +77,45 @@ for file in list_file:
 list_tugas =create_schedule(tugas)
 print len(list_tugas)
 t = threading.Thread(target=animate)
-t.start()
+if(debug_mode==False):
+    t.start()
 
 
 tugas_duplicate=[]
-
+all_duplicate=[]
 for round in list_tugas:
     for match in round:
-        if match[0] in tugas_duplicate or match[1] in tugas_duplicate:
+        #if match[0] in tugas_duplicate or match[1] in tugas_duplicate:
+        if  match[1] in tugas_duplicate:
             pass
-            #print "duplicate"
+            #print "duplicate",match[1]
         elif match[0] == match[1]:
-            pass
             #print "same file"
+            pass
         else:
+            #print match[0] , match[1]
             text1 = docx2txt.process(match[0])
             text2 = docx2txt.process(match[1])
             ratio = SequenceMatcher(None,text1,text2).ratio();
-            if(ratio > 0.5):
-                tugas_duplicate.append(match[0]);
-                tugas_duplicate.append(match[1])
-                #print match[0] + " added to duplicate"
-                #print match[1] +" added to duplicate"
+            if(ratio > ratio_config):
+                if(match[0] not in tugas_duplicate):
+                    tugas_duplicate.append(match[1]);
+                    #tugas_duplicate.append(match[1])
+                    #print match[0] + " added to duplicate"
+                    #print match[1] +" added to duplicate"
 
-done =True
-print
-print "_______________"
-print "total file ", len(list_tugas)
-print "terdapat " ,len(tugas_duplicate) ," file yang diduga plagiat"
-print "file terduga plagiat dipindahkan ke folder plagiat"
+                    ##put all result
+                    all_duplicate.append(match[0])
+                    all_duplicate.append(match[1])
+
+                    #print out information
+                    if(debug_mode):
+                        print "rasio kesamaan ",ratio," : ",os.path.basename(match[0])," with ",os.path.basename(match[1])
+
+
+
+#remove multi duplicate
+tugas_duplicate =list(set(all_duplicate));
 
 folder_plagiat =os.path.join(direktori,"plagiat")
 
@@ -107,4 +128,12 @@ for plagiat in tugas_duplicate:
     shutil.move(plagiat,p)
 
 
+print
+print "_______________"
+print "total file ", len(list_tugas)
+print "terdapat " ,len(tugas_duplicate) ," file yang diduga plagiat"
+print "file terduga plagiat dipindahkan ke folder plagiat"
+
+
+done =True
 
